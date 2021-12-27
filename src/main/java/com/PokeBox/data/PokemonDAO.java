31a385184bf93c65bb33b80f34644fde2289ab16
Raquel S Moura <rqlmoura@yahoo.com>
2021-12-26 22:17:38 -0800

    Update
    Comments Added

--- src/main/java/com/PokeBox/data/PokemonDAO.java
+++ src/main/java/com/PokeBox/data/PokemonDAO.java
@@ -2,6 +2,13 @@ package com.PokeBox.data;
 import com.PokeBox.beans.Pokemon;
 import java.util.Set;
 
+**
+ *The PokemonDAO extends the GenericDAO in order to inherit the CRUD methods, 
+ *and it sets the type of the data to be Pokemon objects.The author uses to methed getByID int value and getByHasItem.
+ *I don't see any problem here since this interface will integrate to the data base.
+ *
+ */
+
 public interface PokemonDAO extends GenericDAO<Pokemon>{
 public Pokemon getByID(int id);
 public Set<Pokemon> getByHasItem();
