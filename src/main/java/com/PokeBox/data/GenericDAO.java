ab3994c43fd5f7d03ac8ae3a707c0e155e02979e
Raquel S Moura <rqlmoura@yahoo.com>
2021-12-26 22:08:16 -0800

    Update
    comments added

--- src/main/java/com/PokeBox/data/GenericDAO.java
+++ src/main/java/com/PokeBox/data/GenericDAO.java
@@ -2,6 +2,12 @@ package com.PokeBox.data;
 
 import java.util.Set;
 
+/**
+ *
+ *   His Generic interface accessing the database using create, read, update, delete plus his own method getByLevel
+ *   It seems well done!
+ */
+
 public interface GenericDAO<D> {
 
 	public int create(D dataToAdd);
